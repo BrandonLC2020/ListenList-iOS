814d73709963c3d74896c5fb22a5955f7d060eea
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 10:42:39 -0400

    adding files

---
+++ ListenList/ListenList/Lists and Grids/CardList.swift
@@ -0,0 +1,18 @@
+//
+//  CardList.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/12/24.
+//
+
+import SwiftUI
+
+struct CardList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardList()
+}
