f4baf01e6832184f82eef3914b75621e07865921
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 18:47:28 -0400

    Update SearchView.swift

--- ListenList/ListenList/Tabs/SearchView.swift
+++ ListenList/ListenList/Tabs/SearchView.swift
@@ -8,10 +8,14 @@
 import SwiftUI
 
 struct SearchView: View {
-    var searchManager : SpotifyAPIManager
+    @State var searchManager : SpotifyAPIManager
     var accessToken : String
     var tokenType : String
-    var cards : [Card]
+    @State var cards : [Card]
+    @State var searchBy = 0
+    @State var searchInput: String = ""
+    @State var searchOutput: String = ""
+    @State var isEditing = false
     
     init(access: String, type: String) {
         self.accessToken = access
@@ -20,13 +24,61 @@ struct SearchView: View {
         self.cards = []
     }
     
+    func reset() {
+        self.cards = []
+        self.searchManager = SpotifyAPIManager(access: self.accessToken, token: self.tokenType)
+
+    }
+    
     var body: some View {
         NavigationView {
             ScrollView() {
                 VStack{
+                    Picker(selection: $searchBy, label: Text("SearchFilter")) {
+                        Text("Album").tag(0)
+                        Text("Artist").tag(1)
+                        Text("Song").tag(2)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(.all, 10)
                     HStack{
-                        // search bar
                         // type filter
+                        
+                        // search bar
+                        TextField("Search...", text: $searchInput, onEditingChanged: { (edit) in
+                            self.isEditing = true
+                        }, onCommit: {
+                            reset()
+                            self.searchOutput = self.searchInput
+                            if (searchBy == 0) {
+                                self.searchManager.searchAlbums(query: searchOutput, type: "album", userCompletionHandler: <#T##(AlbumSearchResponse?) -> Void#>)
+                            } else if (searchBy == 1) {
+
+                            } else {
+
+                            }
+                        })
+                        .padding(7)
+                        .padding(.horizontal, 25)
+                        .background(Color(.systemGray4))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 10)
+                        .onTapGesture {
+                            self.isEditing = true
+                        }
+                        if searchInput.count != 0 {
+                            Button(action: {
+                                self.isEditing = false
+                                self.searchInput = ""
+                                self.searchOutput = ""
+                                reset()
+                                
+                            }) {
+                                Text("Cancel")
+                            }
+                            .padding(.trailing, 10)
+                            .transition(.move(edge: .trailing))
+                        }
                     }
                     CardList(results: cards)
                 }
