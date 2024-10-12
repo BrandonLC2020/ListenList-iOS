ddd005b5b28398b5a08cbaf79c570e0f471376b3
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 15:54:18 -0400

    built out album card

--- ListenList/ListenList/Cards/List Cards/AlbumCard.swift
+++ ListenList/ListenList/Cards/List Cards/AlbumCard.swift
@@ -8,11 +8,85 @@
 import SwiftUI
 
 struct AlbumCard: View {
+    var id: UUID
+    var input: Media
+    var album: Album?
+    
+    let maxHeight: CGFloat = 120
+    
+    init(input: Media) {
+        self.input = input
+        if case let .album(album) = input.input {
+            self.album = album
+        }
+        self.id = UUID()
+    }
+    
+    func artistsToStr() -> String {
+        var result : String = ""
+        for artist in album!.artists {
+            result += artist.name + ", "
+        }
+        let endIndex = result.index(result.endIndex, offsetBy: -2)
+        let truncated = result[..<endIndex]
+        return String(truncated)
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            HStack(alignment: .center) {
+                AsyncImage(url: URL(string: album!.images[0].url)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }
+                .cornerRadius(15.0)
+                .blur(radius: 4.2)
+                .scaledToFill()
+                .frame(maxHeight: maxHeight)
+                .clipped()
+            }.cornerRadius(15.0)
+            HStack {
+                RoundedRectangle(cornerRadius: 15.0)
+                    .foregroundColor(.gray.opacity(0.7))
+                    .cornerRadius(15.0)
+                    .scaledToFill()
+                    .frame(maxHeight: maxHeight)
+                    .clipped()
+            }.cornerRadius(15.0)
+            
+            HStack(alignment: .center) {
+                //album cover
+                AsyncImage(url: URL(string: album!.images[0].url)) { image in
+                    image.resizable()
+                } placeholder: {
+                    ProgressView()
+                }.aspectRatio(1, contentMode: .fit)
+                    .cornerRadius(15.0)
+                    .frame(maxWidth: 90, maxHeight: 90)
+                    .padding(.all)
+                //song title and artist(s) name(s)
+                VStack(alignment: .leading) {
+                    Text(album!.name)
+                        .bold()
+                        .lineLimit(1)
+                    Text(artistsToStr())
+                        .lineLimit(1)
+                }
+                .padding(.trailing)
+                
+                Spacer()
+            }
+            .scaledToFill()
+            .clipped()
+        }
+        .frame(maxWidth: 600, maxHeight: maxHeight, alignment: .center)
+        .clipped()
+        .padding([.leading, .trailing], 10)
     }
 }
 
+
 #Preview {
-    AlbumCard()
+    AlbumCard(input: Media(input: .album(Album(images: [ImageResponse(url: "https://i.scdn.co/image/ab67616d0000b273f76f8deeba5370c98ad38f1c", height: 640, width: 640)], name: "Chemical", release_date: "2023-04-14", artists: [Artist(name: "Post Malone", artistId: "246dkjvS1zLTtiykXe5h60")]))))
 }
