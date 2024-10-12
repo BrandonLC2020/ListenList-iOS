c62ebcde57e61121e06b5a280368b87617d7eea6
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-11 23:57:25 -0400

    added views

---
+++ ListenList/ListenList/Tabs/ListenListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListenListView.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/11/24.
+//
+
+import SwiftUI
+
+struct ListenListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListenListView()
+}
