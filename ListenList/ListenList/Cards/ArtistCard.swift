93353f79417558d59638be76c3be28bb7e3e2935
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 10:14:26 -0400

    added card files

---
+++ ListenList/ListenList/Cards/ArtistCard.swift
@@ -0,0 +1,18 @@
+//
+//  ArtistCard.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/12/24.
+//
+
+import SwiftUI
+
+struct ArtistCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ArtistCard()
+}
---
+++ ListenList/ListenList/Cards/SongCard.swift
@@ -0,0 +1,18 @@
+//
+//  SongCard.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/12/24.
+//
+
+import SwiftUI
+
+struct SongCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SongCard()
+}
