43e9497c5b2d273223a21e80c5dbd13a8198df39
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 18:17:42 -0400

    Update SpotifyAPIManager.swift

--- ListenList/ListenList/Managers/SpotifyAPIManager.swift
+++ ListenList/ListenList/Managers/SpotifyAPIManager.swift
@@ -65,5 +65,49 @@ class SpotifyAPIManager: ObservableObject {
         }).resume()
         //print(result)
     }
+
     
+    func searchArtists(query: String, type: String, userCompletionHandler: @escaping (ArtistSearchResponse?) -> Void) {
+        //print("this is the access token: \(accessToken)")
+        let formattedQuery = query.replacingOccurrences(of: " ", with: "+")
+        let urlStr = "https://api.spotify.com/v1/search?q=\(formattedQuery)&type=\(type)&market=US&limit=50&offset=0"
+        let authorizationAccessTokenStr = accessToken
+        let authorizationTokenTypeStr = tokenType
+        let requestHeaders: [String:String] = ["Authorization" : "\(authorizationTokenTypeStr) \(authorizationAccessTokenStr)"]
+        //print(requestHeaders)
+        var request = URLRequest(url: URL(string: urlStr)!)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = requestHeaders
+        URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            guard
+                let data = data,
+                let response = response as? HTTPURLResponse,
+                error == nil
+            else {
+                print("error", error ?? URLError(.badServerResponse))
+                return
+            }
+            
+            guard (200 ... 299) ~= response.statusCode else {
+                print("statusCode should be 2xx, but is \(response.statusCode)")
+                print("response = \(response)")
+                return
+            }
+            do {
+                //print(data)
+                let responseObject: ArtistSearchResponse = try JSONDecoder().decode(ArtistSearchResponse.self, from: data)
+                //print(responseObject)
+                userCompletionHandler(responseObject)
+                
+            } catch {
+                print(error) // parsing error
+                if let responseString = String(data: data, encoding: .utf8) {
+                    print("responseString = \(responseString)")
+                } else {
+                    print("unable to parse response as string")
+                }
+            }
+        }).resume()
+        //print(result)
+    }
 }
