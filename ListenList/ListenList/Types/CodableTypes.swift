a2ba542108ba57df1fe196520c8e696f5b0de28a
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-11 23:21:52 -0400

    added types

---
+++ ListenList/ListenList/Types/CodableTypes.swift
@@ -0,0 +1,34 @@
+//
+//  CodableTypes.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/11/24.
+//
+    
+
+struct SongResponse: Codable, Hashable {
+    var album: AlbumResponse
+    var artists: [ArtistResponse]
+    var duration_ms: Int //in milliseconds
+    var name: String
+    var popularity: Int
+}
+
+struct ArtistResponse: Codable, Hashable {
+    var images: [ImageResponse]?
+    var name: String
+    var popularity: Int?
+    var id: String
+}
+
+struct AlbumResponse: Codable, Hashable {
+    var images: [ImageResponse]
+    var name: String
+    var release_date: String
+}
+
+struct ImageResponse: Codable, Hashable {
+    var url: String
+    var height: Int?
+    var width: Int?
+}
---
+++ ListenList/ListenList/Types/IdentifiableTypes.swift
@@ -0,0 +1,34 @@
+//
+//  IdentifiableTypes.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/11/24.
+//
+
+import Foundation    
+
+struct Song: Identifiable, Hashable {
+    let id = UUID()
+    var rank: Int?
+    var album: Album
+    var artists: [Artist]
+    var duration_ms: Int //in milliseconds
+    var name: String
+    var popularity: Int
+}
+
+struct Artist: Identifiable, Hashable {
+    let id = UUID()
+    var rank: Int?
+    var images: [ImageResponse]?
+    var name: String
+    var popularity: Int?
+    var artistId: String
+}
+
+struct Album: Identifiable, Hashable {
+    let id = UUID()
+    var images: [ImageResponse]
+    var name: String
+    var release_date: String
+}
