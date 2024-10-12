7795dfef358d45aac61dc05075824d1a2cd0db86
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-10-12 17:20:22 -0400

    Create AudiobookCard.swift

---
+++ ListenList/ListenList/Cards/List Cards/AudiobookCard.swift
@@ -0,0 +1,18 @@
+//
+//  AudiobookCard.swift
+//  ListenList
+//
+//  Created by Brandon Lamer-Connolly on 10/12/24.
+//
+
+import SwiftUI
+
+struct AudiobookCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AudiobookCard()
+}
