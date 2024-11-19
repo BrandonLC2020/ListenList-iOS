56d250fe519606c27201133badd93a180f025eeb
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2024-11-19 17:38:58 -0500

    Update SearchView.swift

--- ListenList/ListenList/Tabs/SearchView.swift
+++ ListenList/ListenList/Tabs/SearchView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct SearchView: View {
-    @State var searchManager: SpotifyAPIManager
+    var searchManager: SpotifyAPIManager
     var accessToken: String
     var tokenType: String
     @State var cards = [Card]()
     @State var searchBy = 0
-    @State var searchText: String = ""
+    @State private var searchText: String = ""
     @State var isLoading = false
     @FocusState private var isTextFieldFocused: Bool // Focus management for TextField
     
@@ -17,61 +17,89 @@ struct SearchView: View {
         self.cards = []
     }
     
-    func reset() {
-        self.cards = []
-    }
-    
-    func performSearch() async {
-        reset()
+    func performSearch() async -> [Card] {
         isLoading = true
         defer { isLoading = false } // Ensure loading state resets
-
+        
         switch searchBy {
-            case 0: await searchAlbums()
-            case 1: await searchArtists()
-            default: await searchSongs()
+            case 0: return await searchAlbums()
+            case 1: return await searchArtists()
+            default: return await searchSongs()
         }
     }
     
-    func searchAlbums() async {
+    func searchAlbums() async -> [Card] {
+        var results = [Card]()
         if let albumSearchResults = try? await searchManager.search(query: searchText, type: "album"),
            let albums = albumSearchResults.albums {
-            DispatchQueue.main.async {
-                self.cards = albums.items.map { album in
-                    let artists = album.artists?.map { Artist(name: $0.name, artistId: $0.id) } ?? []
-                    return Card(input: .album, media: Media(input: .album(Album(images: album.images, name: album.name, release_date: album.release_date, artists: artists))))
-                }
+                results = albums.items.map { album in
+                let artists = album.artists?.map { Artist(name: $0.name, artistId: $0.id) } ?? []
+                return Card(input: .album, media: Media(input: .album(Album(images: album.images, name: album.name, release_date: album.release_date, artists: artists))))
             }
         }
+        return results
     }
     
-    func searchSongs() async {
+    func searchSongs() async -> [Card] {
+        var results = [Card]()
         if let songSearchResults = try? await searchManager.search(query: searchText, type: "track"),
            let songs = songSearchResults.tracks {
-            DispatchQueue.main.async {
-                self.cards = songs.items.map { song in
-                    let albumArtists = song.album.artists?.map { Artist(name: $0.name, artistId: $0.id) } ?? []
-                    let songArtists = song.artists.map { Artist(name: $0.name, artistId: $0.id) }
-                    return Card(input: .song, media: Media(input: .song(Song(album: Album(images: song.album.images, name: song.album.name, release_date: song.album.release_date, artists: albumArtists), artists: songArtists, duration_ms: song.duration_ms, name: song.name, popularity: song.popularity))))
-                }
+                
+                results = songs.items.map { song in
+                let albumArtists = song.album.artists?.map { Artist(name: $0.name, artistId: $0.id) } ?? []
+                let songArtists = song.artists.map { Artist(name: $0.name, artistId: $0.id) }
+                return Card(input: .song, media: Media(input: .song(Song(album: Album(images: song.album.images, name: song.album.name, release_date: song.album.release_date, artists: albumArtists), artists: songArtists, duration_ms: song.duration_ms, name: song.name, popularity: song.popularity))))
+                
             }
         }
+        return results
     }
     
-    func searchArtists() async {
+    func searchArtists() async -> [Card] {
+        var results = [Card]()
         if let artistSearchResults = try? await searchManager.search(query: searchText, type: "artist"),
            let artists = artistSearchResults.artists {
-            DispatchQueue.main.async {
-                self.cards = artists.items.map { artist in
-                    return Card(input: .artist, media: Media(input: .artist(Artist(images: artist.images, name: artist.name, popularity: artist.popularity, artistId: artist.id))))
-                }
+                
+                results = artists.items.map { artist in
+                return Card(input: .artist, media: Media(input: .artist(Artist(images: artist.images, name: artist.name, popularity: artist.popularity, artistId: artist.id))))
+                
             }
         }
+        return results
     }
     
+    func startSearch() async {
+        guard !searchText.isEmpty else { return }
+
+        // Clear previous state
+        DispatchQueue.main.async {
+            isLoading = true
+            cards = [] // Clear previous results
+            isTextFieldFocused = false // Dismiss keyboard
+        }
+
+        // Perform search and handle results
+        let results: [Card] = await performSearch()
+        DispatchQueue.main.async {
+            isLoading = false // Stop loading
+            cards = results // Update results
+        }
+    }
+
+    func resetSearch() {
+        // Reset all states explicitly
+        DispatchQueue.main.async {
+            searchText = ""
+            cards = []
+            isLoading = false
+            isTextFieldFocused = false // Clear keyboard focus
+        }
+    }
+
+
     var body: some View {
         NavigationView {
-            ScrollView{
+            ScrollView {
                 VStack {
                     Picker(selection: $searchBy, label: Text("Search Filter")) {
                         Text("Album").tag(0)
@@ -88,7 +116,7 @@ struct SearchView: View {
                             .focused($isTextFieldFocused)
                             .onSubmit {
                                 Task {
-                                    await performSearch()
+                                    await startSearch()
                                 }
                             }
                             .padding(7)
@@ -99,12 +127,11 @@ struct SearchView: View {
                         
                         if !searchText.isEmpty {
                             Button("Cancel") {
-                                searchText = ""
-                                reset()
-                                isTextFieldFocused = false // Dismiss keyboard
+                                resetSearch()
                             }
                             .foregroundColor(.blue)
                             .padding(.trailing, 10)
+
                         }
                     }
                     
@@ -116,6 +143,12 @@ struct SearchView: View {
                 }
             }
             .navigationTitle("Search")
+            .onTapGesture {
+                isTextFieldFocused = false // Dismiss keyboard when tapping outside
+            }
         }
     }
+
+
+
 }
